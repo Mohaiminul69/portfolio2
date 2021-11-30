6cf272cb99358b3154b32e087a93adac23ae86cc
mohaiminul69 <mohaiminul69@gmail.com>
2021-11-30 16:59:58 +0600

    first commit

---
+++ js/main.js
@@ -0,0 +1,90 @@
+const hamburger = document.querySelector(".hamburger");
+const menu = document.querySelector(".menu");
+const menuItem = document.querySelectorAll(".menu li a");
+const nav = document.querySelector("#main-nav");
+const emailId = document.querySelector("#email");
+// const progressbar = document.querySelector("#progressbar");
+
+// const totalHeight = document.body.scrollHeight - window.innerHeight;
+// window.onscroll = () => {
+//   const progressHeight = (window.pageYOffset / totalHeight) * 100;
+//   progressbar.style.height = progressHeight + "%";
+// };
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  menu.classList.toggle("active");
+});
+
+document.addEventListener("scroll", () => {
+  var scrollPos = window.scrollY;
+  if (scrollPos > 150) {
+    nav.style.background = "rgba(0, 0, 0, 0.95)";
+  } else {
+    nav.style.background = "transparent";
+  }
+});
+
+menuItem.forEach((item) => {
+  item.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    menu.classList.toggle("active");
+  });
+});
+
+var doc = jQuery(document);
+
+doc.ready(function () {
+  "use strict";
+  var typed = new Typed(".typed", {
+    strings: [
+      "a Front-end Web Developer",
+      "Passionate",
+      "Diligent",
+      "Punctual...",
+      "kinda Awesome <span>&#x270c;</span>",
+    ],
+    smartBackspace: true, // Default value
+    loop: true,
+    typeSpeed: 60,
+    backDelay: 2500,
+    fadeOut: true,
+    startDelay: 3000,
+  });
+});
+
+emailId.addEventListener("click", copyText);
+
+function copyText() {
+  const text = emailId.textContent;
+  /* Select the text field */
+  const fakeElement = document.createElement("textarea");
+  fakeElement.value = text;
+  document.body.appendChild(fakeElement);
+  fakeElement.select();
+  fakeElement.setSelectionRange(0, 99999); /* For mobile devices */
+
+  /* Copy the text inside the text field */
+  document.execCommand("copy");
+  document.body.removeChild(fakeElement);
+  /* Alert the copied text */
+  // alert("Copied to Clipboard: " + text);
+}
+
+//smooth scrolling
+//smooth scrolling
+//smooth scrolling
+// const speed = 0.04;
+
+// var offset = 0;
+// function smoothScroll() {
+//   offset += (window.pageYOffset - offset) * speed;
+//   var scroll = `translateY(-${offset}px)`;
+//   body.style.transform = scroll;
+//   callScroll = requestAnimationFrame(smoothScroll);
+// }
+// smoothScroll();
+//smooth scrolling
+//smooth scrolling
+//smooth scrolling
+//smooth scrolling
---
+++ js/mouse.js
@@ -0,0 +1,72 @@
+// Mouse
+// Mouse
+// Mouse
+const body = document.body;
+let mouseCursor = document.querySelector(".cursor");
+const mouseFollower = document.querySelector(".cursor-follower");
+let navLinks = document.querySelectorAll("a");
+let allLinks = document.querySelectorAll("a");
+let imgLinks = document.querySelectorAll(".linked-img");
+
+window.addEventListener("mousemove", cursor);
+
+var cursorDisplay = null;
+function turnOnCursor() {
+  if (cursorDisplay === "block") {
+    return;
+  }
+  cursorDisplay = "block";
+  mouseCursor.style.display = cursorDisplay;
+  mouseFollower.style.display = cursorDisplay;
+}
+
+function cursor(e) {
+  turnOnCursor();
+  mouseCursor.style.top = `${e.pageY}px`;
+  mouseCursor.style.left = `${e.pageX}px`;
+  mouseFollower.style.top = `${e.pageY}px`;
+  mouseFollower.style.left = `${e.pageX}px`;
+}
+
+body.addEventListener("mousedown", (e) => {
+  if (e) {
+    mouseCursor.classList.add("cursorClicked");
+  }
+});
+body.addEventListener("mouseup", (e) => {
+  if (e) {
+    mouseCursor.classList.remove("cursorClicked");
+  }
+});
+body.addEventListener("mouseover", () => {
+  mouseCursor.classList.add("cursor");
+  mouseFollower.classList.add("cursor-follower");
+});
+body.addEventListener("mouseleave", () => {
+  mouseCursor.classList.remove("cursor");
+  mouseFollower.classList.remove("cursor-follower");
+});
+
+allLinks.forEach((link) => {
+  link.addEventListener("mouseover", () => {
+    mouseCursor.classList.add("link-grow");
+    mouseFollower.style.display = "none";
+  });
+  link.addEventListener("mouseleave", () => {
+    mouseFollower.style.display = "block";
+    mouseCursor.classList.remove("link-grow");
+  });
+});
+imgLinks.forEach((link) => {
+  link.addEventListener("mouseover", () => {
+    mouseCursor.classList.add("img-cursor");
+    mouseFollower.style.display = "none";
+  });
+  link.addEventListener("mouseleave", () => {
+    mouseFollower.style.display = "block";
+    mouseCursor.classList.remove("img-cursor");
+  });
+});
+// Mouse
+// Mouse
+// Mouse
